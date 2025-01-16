30a01219b2a3289a5c2e7266a415c49a8ad8c1a0
Harish Rusum <harish.rusum@gmail.com>
2025-01-16 21:21:40 +0530

    added a swap script

---
+++ swap.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+mv themeAlternate.css tempTheme.css
+mv styleAlternate.css tempStyle.css
+mv theme.css themeAlternate.css
+mv style.css styleAlternate.css
+mv tempTheme.css theme.css
+mv tempStyle.css style.css
